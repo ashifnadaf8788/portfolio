860c4b375ce317c4dd2a5406d2b00309136b0801
ashifnadaf <ashifnadaf99>
2022-08-14 20:04:01 +0530

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"resume",component:ResumeComponent},
+  {path:"portfolio",component:PortfolioComponent},
+  {path:"services",component:ServiceComponent},
+  {path:"contact",component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
